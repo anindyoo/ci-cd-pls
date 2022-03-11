8e72e2393e34779e865005632e2787409268ca00
anindyo <anindyo0216@gmail.com>
2022-03-11 13:17:54 +0700

    added index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+var a = 20;
+var b = 5;
+
+function add(x, y) {
+    return x + y;
+}
+function substract(x, y) {
+    return x - y;
+}
+function multiply(x, y) {
+    return x * y;
+}
+function divide(x, y) {
+    return x / y;
+}
+function modulo(x, y) {
+    return x % y;
+}
+
+console.log(add(a, b));
+console.log(substract(a, b));
+console.log(multiply(a, b));
+console.log(divide(a, b));
+console.log(modulo(a, b));
